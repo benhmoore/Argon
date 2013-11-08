8582d5f0f95a082e344c0b3bf5d1f409c5bb92ba
Ben Moore <moore.h.ben@gmail.com>
2013-11-08 17:22:58 -0600

    Revamped Cenny.php
    New Cenny.php to work with Cenny.js.
    
    ***Features:
    1. SELF CONTAINED - no need for any other files or folders, will
    automatically create them.
    2. Simpler / smaller / more secure.

--- server/cenny.php
+++ server/cenny.php
@@ -1,194 +1,161 @@
-<?php 
+<?php
 
-	header('Access-Control-Allow-Origin: *');
+	/* * * * * * * * * * * * * * * * * *
+	 *
+	 * <- Backend for Cenny.js ->
+	 * 
+	 * How data is stored:
+	 *   Backend (root) directory
+	 *      v
+	 *    Group directory
+	 *      v
+	 *     User directory
+	 *      v
+	 *      Data.txt
+	 *
+	 *
+	/* * * * * * * * * * * * * * * * * */
+
+
+	function openFile($url, $size) {
+		if (file_exists($url)) {
+		$myFile = $url;
+		$fh = fopen($myFile, 'r');
+		$dataToReturn = fread($fh, $size);
+		fclose($fh);	
+		} else {
+			saveFile($url, "");
+			$dataToReturn = "";
+		}
+		return $dataToReturn;
 	
 	
-	
-	
-	
-	if (file_exists("backend/")) {} else {
-		mkdir("backend/", 0700);
 	}
 	
-	if (file_exists("src/")) {} else {
-		echo "MAYDAY! src directory does not exist!";		
+	function delete_files($dir) {
+	  if (is_dir($dir)) {
+	    $objects = scandir($dir);
+	    foreach ($objects as $object) {
+	      if ($object != "." && $object != "..") {
+	        if (filetype($dir."/".$object) == "dir") 
+	           rrmdir($dir."/".$object); 
+	        else unlink   ($dir."/".$object);
+	      }
+	    }
+	    reset($objects);
+	    rmdir($dir);
+	  }
+	 }
+
+	function saveFile($url, $dataToSave) {
+		$myFile = $url;
+		$fh = fopen($myFile, 'w') or die($myFile);
+		fwrite($fh, $dataToSave);
+		fclose($fh);
+	
+	
 	}
 	
+	//***********************************************************
+		$group_loggedin = false;
+		$user_loggedin = false;
+	//***********************************************************
 	
-	if (file_exists("backend/info/")) {} else {
-		mkdir("backend/info/", 0700);
-	}
+
+	$action = $_POST['action'];
 	
-	
-	include_once('src/main.php');
-	
-	
-	$location = 1;
-	$data = $_POST['data'];
-	$dataLength = strlen(utf8_decode($data));//the length of the data
+	$clientData = $_POST['data'];
 	
 	//formatting
-		$data = str_replace("\'","'",$data);
-		$data = str_replace('\"','"',$data);
-	
-	$action = $_POST['act'];
-	
-	$user = $_POST['user'];
-	$pass = $_POST['pass'];
-	$readAccess = $_POST['readAccess'];
-	
-	//fixr upr
-	$user = str_replace("http://","",$user);
-	$user = str_replace("/","",$user);
-	$user = str_replace("\\","",$user);
-	$user = str_replace(".","",$user);
-	$user = str_replace(":","",$user);
-	$user = str_replace("-","",$user);
-	$user = str_replace(" ","",$user);
-	$user = str_replace("_","",$user);
-	$user = str_replace(",","",$user);
-	$user = str_replace("#","",$user);
-	$user = str_replace("@","",$user);
-	$user = str_replace("*","",$user);
-	$user = str_replace("<","",$user);
-	$user = str_replace(">","",$user);
-	$user = str_replace("'","",$user);
-	$user = str_replace('"',"",$user);
-	
-	if (file_exists("backend/")) {
-	
-	
-	
-	//check if data contains blacklisted word
-	$inBlacklist = checkBlacklist($data, "backend/info/blacklist.txt");
+		$clientData = str_replace("\'","'",$clientData);
+		$clientData = str_replace('\"','"',$clientData);
 
-	if ($inBlacklist === "false") { 
-	$domainBlocked = openFile("backend/info/access.txt", 5000);
-	if ($domainBlocked !== "block") {
-	if ($action === "get") {//____________________________________________________________________GET____
-			
-			if (file_exists("backend/$user/ps.txt")) {$passwordFromFile = openFile("backend/$user/ps.txt", 5000);}
-			if (file_exists("backend/$user/readAccess.txt")) {$readableX = openFile("backend/$user/readAccess.txt", 5000);}
-				
-			if ($readableX !== "false") {
-				if (file_exists("backend/$user/$location.txt")) {
-				
-						$dataToEcho = openFile("backend/$user/$location.txt", 550000);
-						
-						echo $dataToEcho;
-				
-					} else {
-						echo json_encode(false);
-					}
-			} else {
-				//if password is correct
-			if ($pass === $passwordFromFile) {
-				
-				if (file_exists("backend/$user/$location.txt")) {
-			
-					$dataToEcho = openFile("backend/$user/$location.txt", 550000);
-					
-					echo $dataToEcho;
-			
-				} else {
-					echo json_encode(false);
-				}
-			
-			} //end pass check
-			}//end readable check
-		//ANALYTICS FOR REQUESTS~~~~~~~~~~~~~~~~~
-		plusNum("backend/info/requests.txt", 1);
-	}  else if ($action === "save") {//___________________________________________________________SAVE____
-			if ($location < 501) {
-		//open password
-				if (file_exists("backend/$user/ps.txt")) {$passwordFromFile = openFile("backend/$user/ps.txt", 7000);}
-				
-				//if password is correct
-			if ($pass === $passwordFromFile) {
-		
-					if ($dataLength < 510000 && $dataLength > 0) { 		
-				  		saveFile("backend/$user/$location.txt", $data);
-				  		echo json_encode(true);
-					} else {
-						echo json_encode(false);
-					}	   
-			
-			} else {
-				echo json_encode(false);
-			}
-			}//end location check
-		//ANALYTICS FOR REQUESTS~~~~~~~~~~~~~~~~~
-		plusNum("backend/info/requests.txt", 1);
-		
-	} else if ($action === "auth") {
-		$isBlocked = openFile("backend/info/newUsersBlocked.txt", 7001);
-		if ($isBlocked !== "block") {
-				$accounts = openFile("backend/info/accounts.txt", 7001);
-				$urlFilename = "backend/$user/";
-				if ($accounts < 101) {
-				if (file_exists($urlFilename)) { echo "Group already exists.";}else{mkdir("backend/$user/", 0700);
-			
-				  	saveFile("backend/$user/ps.txt", $pass);
-				  	saveFile("backend/$user/readAccess.txt", $readAccess);
-				 
-					saveFile("backend/info/accounts.txt", $accounts + 1);
-					
-					//save to list
-					addUserToDirectory($user, "backend/info/userList.txt");
-					
-				  echo "Group created.";
-				 
-				  
-				
-				} 	
-				}
-				}
+	$groupName = $_POST['groupName'];
+	$groupKey = $_POST['groupKey'];
 	
-		}else {
-			 echo "Group creation blocked.";
+	$userName = $_POST['userName'];
+	$userPass = $_POST['userPass'];
+
+	$directory = "backend";
+	if (file_exists("backend/")) {} else {
+		mkdir("backend/", 0700);
+		
+	}
+	
+	//#######################################################################################################################################################
+	//############################################################### GROUP / USER ##########################################################################
+	//#######################################################################################################################################################
+	
+	// groups
+	
+	if (file_exists("$directory/$groupName/")) {
+		$orginalKey = openFile("$directory/$groupName/key.txt", 10000);
+		if ($orginalKey === $groupKey) {
+			$group_loggedin = true;
+		} else {
+			echo json_encode("Group key incorrect.");
 		}
 		
-		//ANALYTICS FOR REQUESTS~~~~~~~~~~~~~~~~~
-		plusNum("backend/info/requests.txt", 1);
-		
-	} else if ($action === "deauth") {
-		if ($user !== "default") {
-			$urlFilename = "backend/$user/";
-				if (file_exists($urlFilename)) {
-			
-					$lastPass = openFile("backend/$user/ps.txt", 5000);	
-				
-				} else {
-					 echo "Group does not exist.";
-				}
-				if ($lastPass === $pass) {
-					delete_files("backend/$user/");
-					
-					//remove Group from list
-					removeUserFromDirectory($user, "backend/info/userList.txt");
-					
-				$accounts = openFile("backend/info/accounts.txt", 5000);
-					saveFile("backend/info/accounts.txt", $accounts - 1);
-					echo "Group removed.";
-				} else {
-				    echo "Group key incorrect.";
-                }
+	} else {
+		mkdir("$directory/$groupName/", 0700);
+		saveFile("$directory/$groupName/key.txt", $groupKey);
+			$group_loggedin = true;
+	}
+	
+	// users
+	
+	if (file_exists("$directory/$groupName/$userName/")) {
+		$orginalPass = openFile("$directory/$groupName/$userName/pass.txt", 10000);
+		if ($orginalPass === $userPass) {
+			$user_loggedin = true;
+		} else {
+			echo json_encode("User pass incorrect.");
 		}
-				
-		//ANALYTICS FOR REQUESTS~~~~~~~~~~~~~~~~~
-		plusNum("backend/info/requests.txt", 1);
 		
-	} //end actions
+	} else {
+		mkdir("$directory/$groupName/$userName/", 0700);
+		saveFile("$directory/$groupName/$userName/pass.txt", $userPass);
+			$user_loggedin = true;
+	}
 	
-	} else { //end DOMAINBLOCK	
-		echo json_encode("Domain locked.");
-	}//end DOMAINBLOCK else	
 	
-	}//end user blacklist check
-
+	
+	//########################################################################################################################
+	if ($group_loggedin === true && $user_loggedin === true) {
 		
+		// - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - /
+		if ($action === "get") {
+			
+			$openedData = openFile("$directory/$groupName/$userName/data.txt", 500000);
+			echo $openedData;
+		
+		} else if ($action === "set") {
+		
+			saveFile("$directory/$groupName/$userName/data.txt", $clientData);
+			echo json_encode("saved to user " . $userName . " in group " . $groupName);
+			
+		} else if ($action === "removeUser") {
+		
+			delete_files("$directory/$groupName/$userName/");
+			echo json_encode("removed user " . $userName . " in group " . $groupName);
+			
+		} else if ($action === "removeGroup") {
+		
+			delete_files("$directory/$groupName/");
+			echo json_encode("removed group " . $groupName . " and all users within.");
+			
+		}
+		// - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - / - /
+	
+	
+	} else {
+	//user or group incorrect.	
+	}	
+	//########################################################################################################################
+	
+	
+	
+	
 
-	
-	
-	
+
 ?>
